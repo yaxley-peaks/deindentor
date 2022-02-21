e4e7e3352cfce572381bf5f90e3715c4c082ded2
yaxley-peaks <epiclycoolgaemer@gmail.com>
2022-02-21 14:03:38 +0530

    can use command line args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use std::fs::File;
 use std::io::Write;
-struct KV {
-    k: usize,
-    v: String,
+use std::fs;
+use clap::Parser;
+struct Line {
+    indent: usize,
+    line: String,
+}
+
+#[derive(Parser,Debug)]
+struct CLI {
+    #[clap(parse(from_os_str))]
+    input: std::path::PathBuf,
+    #[clap(parse(from_os_str))]
+    output: std::path::PathBuf,
 }
 
 fn determine_indent(string: &str) -> usize {
@@ -21,18 +31,18 @@ fn find_max_indent(lines: &str) -> usize {
     for line in lines.split('\n') {
         indents.push(determine_indent(line));
     }
-    *indents.iter().max().unwrap()
+    indents.into_iter().max().unwrap()
 }
 fn spaces(number: usize) -> String {
     let x = [' '; 100];
     x.iter().take(number).collect()
 }
-fn generate_indents(lines: &str) -> Vec<KV> {
-    let mut kv: Vec<KV> = Vec::new();
+fn generate_indents(lines: &str) -> Vec<Line> {
+    let mut kv: Vec<Line> = Vec::new();
     for line in lines.split('\n') {
-        kv.push(KV {
-            k: determine_indent(line),
-            v: line.trim().to_string(),
+        kv.push(Line {
+            indent: determine_indent(line),
+            line: line.trim().to_string(),
         });
     }
     kv
@@ -42,13 +52,14 @@ fn generate_result(lines: &str) -> String {
     let indents = generate_indents(&lines);
     let mut res = Vec::new();
     for item in indents {
-        res.push(String::from(spaces(max_indent - item.k) + &item.v));
+        res.push(String::from(spaces(max_indent - item.indent) + &item.line));
         res.push("\n".to_string());
     }
     res.into_iter().collect()
 }
 fn main() {
-    let file = include_str!("main.rs");
-    let mut outfile = File::create("out.txt").unwrap();
-    writeln!(&mut outfile,"{}", generate_result(file)).unwrap();
+    let args = CLI::parse();
+    let file = fs::read_to_string(&args.input).unwrap();
+    let mut outfile = File::create(&args.output).unwrap();
+    writeln!(&mut outfile,"{}", generate_result(&file)).unwrap();
 }
